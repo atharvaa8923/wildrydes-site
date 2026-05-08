045562ff179d4095d7636c18c6d376b58b4a2c79
ATHARV_KULKARNI <144601698+atharvaa8923@users.noreply.github.com>
2026-05-08 18:46:44 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_btQCVGK8a', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'k2bf3apscl2a1f9cm1nog9aus, // 
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_LO7BawsoK',
+        userPoolClientId: '5as16bhpqhc1344vln675ihj04',
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // keep empty for now (we will fill after API Gateway step)
     }
 };
