813c4f68be0551df86ce90bb4024fe3ee6de83e8
ATHARV_KULKARNI <144601698+atharvaa8923@users.noreply.github.com>
2026-05-08 19:16:41 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1'
     },
     api: {
-        invokeUrl: '' // keep empty for now (we will fill after API Gateway step)
+        invokeUrl: 'https://mteb1oomv2.execute-api.us-east-1.amazonaws.com/prod' // keep empty for now (we will fill after API Gateway step)
     }
 };
